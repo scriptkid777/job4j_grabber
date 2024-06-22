7fcf32e93287906b420e551611cd7cf1b8f67fa7
scriptkid777 <servsystem@yandex.ru>
2024-06-22 22:39:33 +0700

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/quartz/Store.java
+++ src/main/java/ru/job4j/quartz/Store.java
@@ -1,11 +1,12 @@
 package ru.job4j.quartz;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public interface Store {
+public interface Store extends AutoCloseable {
     void save(Post post);
 
-    List<Post> getAll();
+    List<Post> getAll() throws SQLException;
 
     Post findById(int id);
 
